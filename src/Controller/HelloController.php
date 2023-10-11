b5e3c67276f00126811fab3d98b2ddaadfc449b0
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2023-10-11 11:35:52 +0200

    Ajout de conditions sur les paramètres 11.2 fonctionel

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -16,15 +16,20 @@ class HelloController extends AbstractController
         ]);
     }
 
+    #[Route('/hello/{name}/{times}', requirements: ['times' => '\d+'])]
+    public function manyTimes(string $name, int $times = 3): Response
+    {
+        if ($times > 10 or $times <= 0) {
+            $times = 3;
+        }
+
+        return $this->render('hello/many_times.html.twig', ['name' => $name, 'times' => $times]);
+    }
+
+    // les routes sont check dans l'ordres declarée
     #[Route('/hello/{name}')]
     public function world(string $name): Response
     {
         return $this->render('hello/world.html.twig', ['name' => $name]);
-    }
-
-    #[Route('/hello/{name}/{times}')]
-    public function manyTimes(string $name, int $times): Response
-    {
-        return $this->render('hello/many_times.html.twig', ['name' => $name, 'times' => $times]);
     }
 }
