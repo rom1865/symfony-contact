b69e3303e977dfd50610a1142d80d1858811d3a1
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2023-10-25 10:16:17 +0200

    Des routes pour produire des URL 15.6 lien d'un contact fonctionnel TEST : OK

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -13,5 +13,14 @@ class IndexCest
         $I->seeInTitle('Liste des contacts');
         $I->see('Liste des contacts', 'h1');
         $I->seeNumberOfElements('li', 195);
+        $I->seeNumberOfElements('a', 195);
+    }
+
+    public function premierLienDeLaListeRouteCorrect(ControllerTester $I): void
+    {
+        $I->amOnPage('/contact');
+        $I->seeResponseCodeIsSuccessful();
+        $I->click('Andre, Sébastien');
+        $I->seeCurrentRouteIs('app_contact_show');
     }
 }
