592e813059db71cde66592016ec4be140ecb3816
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2023-12-06 11:54:53 +0100

    21.4 Validation des données

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
@@ -14,15 +15,50 @@ class Contact
     private ?int $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 30,
+        minMessage: 'Your first name must be at least {{ limit }} characters long',
+        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
+    )]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 40)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 40,
+        minMessage: 'Your first name must be at least {{ limit }} characters long',
+        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
+    )]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 100,
+        minMessage: 'Your last name must be at least {{ limit }} characters long',
+        maxMessage: 'Your last name cannot be longer than {{ limit }} characters',
+    )]
+    #[Assert\Email(
+        message: 'The email {{ value }} is not a valid email.',
+    )]
     private ?string $email = null;
 
     #[ORM\Column(length: 20)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 20,
+        minMessage: 'Your phone must be at least {{ limit }} characters long',
+        maxMessage: 'Your phone cannot be longer than {{ limit }} characters',
+    )]
+    #[Assert\Regex(
+        pattern: '/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4})$/',
+        message: 'Format de téléphone invalide'
+    )]
     private ?string $phone = null;
 
     #[ORM\ManyToOne(inversedBy: 'contacts')]
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -7,6 +7,8 @@ use App\Entity\Contact;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +17,21 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('phone')
-            ->add('category')
+            ->add('firstname', null, [
+                'empty_data' => '',
+            ])
+            ->add('lastname', null, [
+                'empty_data' => '',
+            ])
+            ->add('email', EmailType::class, [
+                'empty_data' => '',
+            ])
+            ->add('phone', TelType::class, [
+                'empty_data' => '',
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
+                'placeholder' => 'Catégorie ?',
                 'choice_label' => 'name',
                 'required' => false,
                 'query_builder' => function (EntityRepository $entityRepository) {
