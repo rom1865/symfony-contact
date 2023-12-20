cbb69dc5ad0e1d2ed03cfb683f98aa290ee31ff4
crev0004 <romain.crevet@etudiant.univ-reims.fr>
2023-12-20 10:53:19 +0100

    22.6 Mise à jour des tests a faire

--- tests/Controller/Contact/UpdateCest.php
+++ tests/Controller/Contact/UpdateCest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Entity\User;
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
@@ -18,5 +20,15 @@ class UpdateCest
 
         $I->seeInTitle('Édition de Simpson, Homer');
         $I->see('Édition de Simpson, Homer', 'h1');
+
+        $cretionCmpt = UserFactory::createOne([
+            'roles' => ['ROLE_ADMIN'],
+            'firstname' => 'himiko',
+            'lastname' => 'toga',
+            'email' => 'himiko@example.com',
+            'password' => 'test',
+        ]);
+        $admin = $cretionCmpt->object();
+        $I->amLoggedInAs($admin);
     }
 }
